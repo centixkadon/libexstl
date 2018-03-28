9294003468b20aaa6ae9d759b57f8be0ca0718cf
centixkadon <centixkadon@gmail.com>
2018-03-28 15:55:03 +0800

    Update thread/list

--- test/main.cpp
+++ test/main.cpp
@@ -3,10 +3,31 @@
 
 using namespace std;
 
+void thread_push(thread_list<size_t> & p) {
+  for (size_t i = 0; i < 10; ++i) {
+    p.push_back(i);
+  }
+  p.push_back(-1);
+}
+
+void thread_pop(thread_list<size_t> & p) {
+  while (true) {
+    size_t i = p.front();
+    p.pop_front();
+    if (i == (size_t)-1) break;
+    cout << i << endl;
+  }
+}
+
 int main() {
   cout << "exstl test" << endl;
 
   cout << vector<int>{1, 2, 3, 5, 7, 11} << endl;
+
+  thread_list<size_t> p;
+  thread th1(thread_push, ref(p)), th2(thread_pop, ref(p));
+  th1.join();
+  th2.join();
 
   return 0;
 }
